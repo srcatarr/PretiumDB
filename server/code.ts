ef235e882c41afaab9b342693cead95bafba8c34
Yasir Eymen Kayabaşı <yasirator04@gmail.com>
2023-11-29 12:59:48 +0300

    Hello World!

---
+++ server/code.ts
@@ -0,0 +1,9 @@
+// Setting constant variables
+
+const ss = SpreadsheetApp.openById("");
+const ssp = ss.getSheetByName("");
+
+// Functions for server
+
+function get() {}
+function set() {}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Import requirements
+
+const express = require("express");
+const app = express();
+
+// Set application port
+
+app.listen(3000);
+
+// Get API route
+
+app.get("", (req, res) => {});
